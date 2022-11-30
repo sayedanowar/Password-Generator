2840bf252cdffb629508fa3c2e21bea688027842
Sayed Anowar <108403588+sayedanowar@users.noreply.github.com>
2022-11-30 12:16:22 +0530

    Add files via upload

---
+++ Password_Gen.py
@@ -0,0 +1,24 @@
+
+import random
+import pyperclip
+
+container = '''0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''*6
+
+while True:
+    length = int(input("\nPassword Length\n\n=> "))
+    
+    if length <= 500:
+        password = "".join(random.sample(container, length))
+        print(f"\nYour Password Is : {password}")
+        copy = input("\nCopy To Clipboard (Y/n) : ").upper()
+        if copy == "Y":
+            pyperclip.copy(password)
+            print("\nCopied Successfully!")
+    
+    else:
+        print("\nMAX Length 500")
+
+    new = input("\nGenerate New Password (Y/n) : ").upper()
+    if new != "Y":
+        break
+
